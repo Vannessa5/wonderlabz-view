9cf49533856c3635f54073c87c15d90107011fa8
your_github_username <vanematenga@gmail.com>
2022-06-07 18:05:58 +0200

    angular front-end

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://wonder-bank.herokuapp.com'
+export const environment = {
+  production: true,
+  LOGIN_URL: `${BASE_URL}/api/authenticate`,
+  CUSTOMERS_URL: `${BASE_URL}/api/customers`,
+  USER_URL: `${BASE_URL}/api/user`,
+};
